f16ac41150a7e10f1b39ade7aece03119b87343c
Takechi Kenta <mobile.kenta.takechi@gmail.com>
2022-10-21 15:42:35 +0000

    モーダル完成(draggableにできない-2)

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,7 +2,7 @@ $(function () {
   $("#modal-open").click(function () {
     $("#modal-overlay, #modal-window").fadeIn("fast");
   });
-  $("#modal-window").draggable();
+  $("#modal-window").draggable(); // ドラッグできるようにならない。ここの書き方が不明
   $("#modal-overlay").click(function () {
     $("#modal-overlay, #modal-window").fadeOut("fast");
   });
