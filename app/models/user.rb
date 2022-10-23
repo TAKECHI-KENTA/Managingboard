80d42f71a26c0a274651b9a5c1006a784fd598e3
Kenta Takechi <mobile.kenta.takechi@gmail.com>
2022-10-23 23:16:18 +0000

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
                     }
   
   has_secure_password
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{8,32}\z/i  #https://techtechmedia.com/password-validate-expression/ で確認
+  validates :password, presence: true,
+                       format: { with: VALID_PASSWORD_REGEX
+                       }  #, message: "パスワードは半角8~32文字のアルファベット・数字それぞれ１文字以上含む必要があります" を入れたい
+                       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'boards#index'
   get 'boards/setting'
   get 'boards/index'
 
   resources :users
   resources :comments
+  
+  get '/login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
